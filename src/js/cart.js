abf76a2095989614a2c7149933dc8c102d7655bb
kirstennicole422 <97316728+kirstennicole422@users.noreply.github.com>
2026-09-19 17:44:58 -0600

    added indication of discouunt

--- src/js/cart.js
+++ src/js/cart.js
@@ -11,22 +11,39 @@ function renderCartContents() {
 }
 
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-  <span class="remove-item" data-id="${item.Id}">X</span>
-</li>`;
-
+  if(item.FinalPrice < item.SuggestedRetailPrice) {
+    const newItem = `<li class="cart-card divider">
+    <a href="#" class="cart-card__image">
+      <img
+        src="${item.Image}"
+        alt="${item.Name}"
+      />
+    </a>
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__price">$${item.FinalPrice} (discounted!)</p>
+    <span class="remove-item" data-id="${item.Id}">X</span>
+  </li>`;
+  else{  
+    const newItem = `<li class="cart-card divider">
+    <a href="#" class="cart-card__image">
+      <img
+        src="${item.Image}"
+        alt="${item.Name}"
+      />
+    </a>
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+    <span class="remove-item" data-id="${item.Id}">X</span>
+  </li>`;
+  }
   return newItem;
 }
 
